1ba20a8ae7e7d40ea9b836da728483fc3a555d6f
MaryneZa <maryne6916@gmail.com>
2025-05-23 20:48:49 +0700

    user repo implement

--- internal/adapter/storage/mongo/repository/user.go
+++ internal/adapter/storage/mongo/repository/user.go
@@ -1,14 +1,88 @@
 package repository
 
 import (
+	"context"
+	"github.com/MaryneZa/backend-challenge/internal/core/domain"
 	"github.com/MaryneZa/backend-challenge/internal/core/port"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type UserRepository struct {
-
+	collection *mongo.Collection
 }
 
-func NewUserRepository() port.UserRepository {
-	return &UserRepository{}
+func NewUserRepository(db *mongo.Database) port.UserRepository {
+	return &UserRepository{collection: db.Collection("users")}
+}
+
+func (ur *UserRepository) Create(ctx context.Context, user *domain.User) error {
+
+	data := bson.D{
+		{Key: "email", Value: user.Email},
+		{Key: "password", Value: user.Password},
+	}
+	if _, err := ur.collection.InsertOne(ctx, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *UserRepository) FindByID(ctx context.Context, id bson.ObjectID) (*domain.User, error) {
+	var user domain.User
+	filter := bson.D{{Key: "_id", Value: id}}
+	if err := ur.collection.FindOne(ctx, filter).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *UserRepository) FindByEmail(ctx context.Context, email string) (*domain.User, error) {
+	var user domain.User
+	filter := bson.D{{Key: "email", Value: email}}
+	if err := ur.collection.FindOne(ctx, filter).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ur *UserRepository) GetAllUser(ctx context.Context) ([]*domain.User, error) {
+	cursor, err := ur.collection.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	var users []*domain.User
+	if err = cursor.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (ur *UserRepository) UpdateEmail(ctx context.Context, id bson.ObjectID, email string) error {
+	filter := bson.D{{Key: "_id", Value: id}}
+	update := bson.D{{"$set", bson.D{{Key: "email", Value: email}}}}
+	if _, err := ur.collection.UpdateOne(ctx, filter, update); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *UserRepository) UpdateName(ctx context.Context, id bson.ObjectID, name string) error {
+	filter := bson.D{{Key: "_id", Value: id}}
+	update := bson.D{{"$set", bson.D{{Key: "name", Value: name}}}}
+	if _, err := ur.collection.UpdateOne(ctx, filter, update); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (ur *UserRepository) Delete(ctx context.Context, id bson.ObjectID) error {
+	filter := bson.D{{Key: "_id", Value: id}}
+	if _, err := ur.collection.DeleteOne(ctx, filter); err != nil {
+		return err
+	}
+	return nil
 }
